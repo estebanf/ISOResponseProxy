0eded10807b601e9b33f1146edb971ab6b19f371
April Eggers <april.eggers@flatironsjouve.com>
2017-10-13 22:39:02 -0600

    Updated the soap proxy to use the xml provided, if any, and to default to the sample response otherwise

--- config.js
+++ config.js
@@ -13,6 +13,9 @@ config.bpms.pool_name="ISO_Response_Manager_ISO";
 config.bpms.iso_response_wsdl = config.bpms.server + "everteam/ode/processes/"+config.bpms.project_path + "_" +
     config.bpms.process_name +"_" + config.bpms.pool_name + "?wsdl";
 
-config.bpms.wsdl_service_name="Receive_ISO_ResponseRequest";
+config.bpms.namespace_prefix = "iso";
+config.bpms.namespace = "\"http://bpms.everteam.com/Processes/Core/ProcessISOResponse/ISO_Response_Manager\"";
+
+config.bpms.wsdl_request_element= config.bpms.namespace_prefix + ":Receive_ISO_ResponseRequest";
 
 module.exports = config;
